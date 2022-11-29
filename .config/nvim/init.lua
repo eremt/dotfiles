b18468635bca49ef4e48be818330597ad93c1c12
Troy <eremtroy@gmail.com>
2022-11-29 19:15:18 +0000

    .vim, .config/nvim: add minimal vimrc and init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- syntax highlighting
+-- syntax on default
+vim.cmd('colorscheme boring')
+
+-- indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- disable swapfile
+vim.opt.swapfile = false
