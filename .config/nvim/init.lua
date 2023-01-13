05d40279526684e4ab0af215abbff425ee276b77
Troy <eremtroy@gmail.com>
2023-01-13 20:27:15 +0000

    .local/bin, .vim, .config/nvim: add fzf to core packages, set vim/neovim leader to <space> and command mode + search replace keybindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,12 +10,20 @@ vim.opt.expandtab = true
 -- disable swapfile
 vim.opt.swapfile = false
 
+-- set leader to <space>
+vim.g.mapleader = " "
+-- enter command mode
+vim.keymap.set('', '<leader>;', ':', { noremap = true })
+-- search replace
+vim.keymap.set('n', '<leader>/', 'yiw:%s/<C-r>"/', { noremap = true })
+vim.keymap.set('v', '<leader>/', 'y:%s/<C-r>"/', { noremap = true })
+
 -- disable yank when pasting in visual mode
-vim.cmd('vnoremap p "_dP')
+vim.keymap.set('v', 'p', '"_dP', { noremap = true })
 
 -- FZF
 vim.cmd('set rtp+=~/.local/share/nvim/site/pack/plugins/start/fzf')
-vim.cmd(':noremap <c-p> :FZF<CR>')
+vim.keymap.set('n', '<c-p>', ':FZF<CR>', { noremap = true })
 
 -- neovim only
 require('user/lsp')
